5d34cdb675d5578f6d4ac20233ac86e48a77913b
Lenovo <Lenovo@DESKTOP-TED31T3>
2017-09-05 13:33:44 +0700

    lap1

---
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections; // to use Stack
+namespace CPE200Lab1
+{
+    class RPNCalculatorEngine : CalculatorEngine
+    {
+        public void testStackMethod()
+        {
+            Stack testStack = new Stack();
+            testStack.Push("1st element");
+            testStack.Push("2nd element");
+            testStack.Pop();
+            Console.Out.WriteLine(testStack.Pop());
+        }
+        
+
+    }
+}
