7caca8ed1c1c22c03d9ce5a07fd402f81f851dd9
Lenovo <Lenovo@DESKTOP-TED31T3>
2017-09-19 16:00:50 +0700

    finish

--- CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
@@ -8,7 +8,7 @@ namespace CPE200Lab1
 {
     public class CalculatorEngine
     {
-        protected bool isNumber(string str)
+        protected bool isNumber(string str) // protected = class ที่สืบอดไปได้
         {
             double retNum;
             return Double.TryParse(str, out retNum);
--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -7,15 +7,20 @@ using System.Threading.Tasks;
 namespace CPE200Lab1
 {
     public class RPNCalculatorEngine : CalculatorEngine
-    {
+    {       string result;
         public new string Process(string str)
         {
             if (str == null || str == "")
-                return "E";
+            { return "E"; }
+            if (str == "0")
+            {
+                return "0";
+            }
+            
 
             Stack<string> rpnStack = new Stack<string>();
             List<string> parts = str.Split(' ').ToList<string>();
-            string result;
+            
             string firstOperand, secondOperand;
 
             foreach (string token in parts)
@@ -23,44 +28,55 @@ namespace CPE200Lab1
                 if (isNumber(token))
                 {
                     int i;
-                    for ( i=0; i<token.Length; i++)
+                    for (i = 0; i < token.Length; i++)
                     {
                         if (token[i] == '+')
                         {
                             return "E";
                         }
                     }
-                    
+
                     rpnStack.Push(token);
                 }
                 else if (isOperator(token))
                 {
                     //FIXME, what if there ==only one left in stack?
-                    if(rpnStack.Count >=2)
+                    try
                     {
                         secondOperand = rpnStack.Pop();
                         firstOperand = rpnStack.Pop();
-                        if (isOperator(firstOperand) || isOperator(secondOperand))
-                            return "E";
-                     }
-                   else
+                        result = calculate(token, firstOperand, secondOperand, 4);
+                        if (result == "E")
+                        {
+                            return result;
+                        }
+                        rpnStack.Push(result);
+                    }
+                    catch
                     {
                         return "E";
                     }
-                    result = calculate(token, firstOperand, secondOperand, 4);
-                    if (result =="E")
-                    {
-                        return result;
-                    }
-                    rpnStack.Push(result);
                 }
+
+                else if (token != "")
+                {
+                    return "E";
+                }
+                   // rpnStack.Push(result);
+                
             }
             //FIXME, what if there ==more than one, or zero, items in the stack?
-            if (rpnStack.Count != 0)
-                result = rpnStack.Pop();
+            if (rpnStack.Count() == 0 || rpnStack.Count() > 1)
+            {
+                return "E";
+            }
             else
-                result = "E";
-            return result;
+            {
+                result = rpnStack.Pop();
+                return result;
+            }
+                 //result = rpnStack.Pop();
+            
         }
     }
 }
